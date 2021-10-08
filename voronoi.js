56170455749cfc3602bff9422888d1f3cf3edaa3
Sam Zhang <shzh8108@bit-locals-MacBook-Pro.local>
2021-10-08 12:02:03 -0600

    webgazer

--- voronoi.js
+++ voronoi.js
@@ -1,12 +1,63 @@
 import { Delaunay } from "d3-delaunay";
 
-export let showVoronoi = true;
+export let showVoronoi = false;
 let visitedCell, oldLink, oldLinkStyle;
-let handleMouseClickVoronoiGlobal, handleMouseMoveVoronoiGlobal;
+let prediction;
+let handleMouseClickVoronoiGlobal, delaunay
+let voronoiCellToElement, pixelToCanvas, ctx, voronoi, linkDisplay
+
+function handleEyeMoveVoronoi() {
+    if (prediction === undefined) {
+        console.log("Undefined prediction")
+        return
+    }
+    const bounded = webgazer.util.bound(prediction);
+    const transformed = pixelToCanvas(bounded.x, bounded.y);
+
+    let cell = delaunay.find(transformed.x, transformed.y - document.body.getBoundingClientRect().top)
+
+    console.log({prediction, bounded, transformed, cell, ctx})
+
+    if (visitedCell != cell) {
+        if (visitedCell !== undefined) {
+            let oldLink = voronoiCellToElement.get(visitedCell);
+            oldLink.style['outline'] = oldLinkStyle;
+
+            if (showVoronoi) {
+                ctx.strokeStyle='black';
+                ctx.beginPath();
+                voronoi.renderCell(visitedCell, ctx);
+                ctx.stroke();
+            }
+        }
+
+        let newLink = voronoiCellToElement.get(cell);
+        oldLink = newLink;
+        oldLinkStyle = newLink.style['outline'] || 'none';
+        newLink.style['outline'] = '5px solid black';
+
+        if (newLink.tagName == 'A') {
+            linkDisplay.innerHTML = newLink.href;
+        }
+        else if (newLink.tagName == 'INPUT') {
+            linkDisplay.innerHTML = newLink.value;
+        }
+
+        if (showVoronoi) {
+            ctx.strokeStyle='red';
+            ctx.beginPath();
+            voronoi.renderCell(cell, ctx);
+            ctx.stroke();
+        }
+    }
+
+    visitedCell = cell;
+}
+
 
 export function drawVoronoi() {
     eraseVoronoi();
-    let linkDisplay = document.createElement("div");
+    linkDisplay = document.createElement("div");
     linkDisplay.setAttribute("id", "attachedLinkOverlayForDelaunay")
     linkDisplay.style['outline'] = '5px solid black';
     linkDisplay.style['position'] = 'fixed';
@@ -39,17 +90,15 @@ export function drawVoronoi() {
     let canvasHeight = canvas.height;
     document.body.appendChild(canvas)
 
-    let ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
     let canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
     ctx.fillStyle = "red";
     //console.log(canvasWidth, canvasHeight, canvasPixelWidth, canvasPixelHeight)
 
-    function pixelToCanvas(x, y) {
-        return {
-            x: canvasWidth * (x / canvasPixelWidth),
-            y: canvasHeight * (y / canvasPixelHeight)
-        };
-    }
+    pixelToCanvas = (x, y) => ({
+        x: canvasWidth * (x / canvasPixelWidth),
+        y: canvasHeight * (y / canvasPixelHeight)
+    })
 
     let points = [];
     let pointsWithNodes = [];
@@ -71,15 +120,9 @@ export function drawVoronoi() {
         pointsWithNodes.push([canvaspts.x, canvaspts.y, node])
     }
 
-    let delaunay = Delaunay.from(points);
-    /*
-    ctx.beginPath();
-    ctx.lineWidth = 0.5;
-    delaunay.render(ctx);
-    ctx.stroke();
-    */
+    delaunay = Delaunay.from(points);
 
-    let voronoi = delaunay.voronoi([1, 1, canvasWidth, canvasHeight])
+    voronoi = delaunay.voronoi([1, 1, canvasWidth, canvasHeight])
     if (showVoronoi) {
         ctx.beginPath();
         ctx.lineWidth = 3;
@@ -87,7 +130,7 @@ export function drawVoronoi() {
         ctx.stroke();
     }
 
-    let voronoiCellToElement = new Map();
+    voronoiCellToElement = new Map();
     pointsWithNodes.forEach((entry) => {
         let x = entry[0];
         let y = entry[1];
@@ -95,53 +138,6 @@ export function drawVoronoi() {
         let cell = delaunay.find(x, y)
         voronoiCellToElement.set(cell, node)
     });
-
-    function handleMouseMoveVoronoi(event) {
-        let transformed = pixelToCanvas(event.layerX, event.layerY);
-        let cell = delaunay.find(transformed.x, transformed.y)
-
-        // for tablets, a second touch means visit
-        if (event.type == 'touchstart' && visitedCell == cell) {
-            let node = voronoiCellToElement.get(cell)
-
-            node.click();
-        }
-
-        if (visitedCell != cell) {
-            if (visitedCell !== undefined) {
-                let oldLink = voronoiCellToElement.get(visitedCell);
-                oldLink.style['outline'] = oldLinkStyle;
-
-                if (showVoronoi) {
-                    ctx.strokeStyle='black';
-                    ctx.beginPath();
-                    voronoi.renderCell(visitedCell, ctx);
-                    ctx.stroke();
-                }
-            }
-
-            let newLink = voronoiCellToElement.get(cell);
-            oldLink = newLink;
-            oldLinkStyle = newLink.style['outline'] || 'none';
-            newLink.style['outline'] = '5px solid black';
-
-            if (newLink.tagName == 'A') {
-                linkDisplay.innerHTML = newLink.href;
-            }
-            else if (newLink.tagName == 'INPUT') {
-                linkDisplay.innerHTML = newLink.value;
-            }
-
-            if (showVoronoi) {
-                ctx.strokeStyle='red';
-                ctx.beginPath();
-                voronoi.renderCell(cell, ctx);
-                ctx.stroke();
-            }
-        }
-
-        visitedCell = cell;
-    }
 
     function handleMouseClickVoronoi(event) {
         let transformed = pixelToCanvas(event.layerX, event.layerY);
@@ -151,19 +147,18 @@ export function drawVoronoi() {
         node.click();
     }
 
-    handleMouseMoveVoronoiGlobal = handleMouseMoveVoronoi;
     handleMouseClickVoronoiGlobal = handleMouseClickVoronoi;
 
+    webgazer.setGazeListener(updateGlobalEyePosition)
     window.addEventListener("resize", drawVoronoi)
-    window.addEventListener("mousemove", handleMouseMoveVoronoiGlobal);
-    window.addEventListener("touchstart", handleMouseMoveVoronoiGlobal);
-    window.addEventListener("click", handleMouseClickVoronoiGlobal);
+    //window.addEventListener("touchstart", handleMouseMoveVoronoiGlobal);
+    //window.addEventListener("click", handleMouseClickVoronoiGlobal);
 };
 
 export function eraseVoronoi() {
-    window.removeEventListener("mousemove", handleMouseMoveVoronoiGlobal);
-    window.removeEventListener("touchstart", handleMouseMoveVoronoiGlobal);
-    window.removeEventListener("click", handleMouseClickVoronoiGlobal);
+    //window.removeEventListener("touchstart", handleMouseMoveVoronoiGlobal);
+    webgazer.clearGazeListener() 
+    //window.removeEventListener("click", handleMouseClickVoronoiGlobal);
     window.removeEventListener("resize", drawVoronoi)
 
     if (oldLink !== undefined) {
@@ -183,5 +178,33 @@ export function toggleVoronoi() {
     drawVoronoi();
 }
 
+export function updateGlobalEyePosition(data, elapsedTime) {
+    if (data == null) {
+        return;
+    }
+
+    prediction = data;
+    
+    if (delaunay !== undefined) {
+        handleEyeMoveVoronoi()
+    }
+}
+
+export function initWebgazer() {
+    webgazer
+        .setRegression("ridge")
+        .setTracker('TFFacemesh')
+        .setGazeListener(updateGlobalEyePosition)
+        .begin();
+    webgazer.showVideoPreview(true)
+        .showFaceOverlay(true)
+        .showFaceFeedbackBox(true)
+        .showPredictionPoints(true)
+        .applyKalmanFilter(true)
+}
+
 drawVoronoi();
+
+window.saveDataAcrossSessions = true;
+window.addEventListener("load", initWebgazer)
 window.addEventListener("load", drawVoronoi);
